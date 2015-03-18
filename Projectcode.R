dd273ccea53742650228ccb2340f24e865bbf871
Eunice Choi <euniceschoi93@gmail.com>
2015-03-18 22:47:25 +0000

    Threat Level Analysis by Continent

--- Projectcode.R
+++ Projectcode.R
@@ -24,3 +24,31 @@ ThreatLevel <- c("LeastConcerned", "NearThreatened", "Vulnerable", "Endangered",
 ggplot(data=df, aes(x = ThreatLevel, y=Count)) + 
   scale_x_discrete(limits = ThreatLevel) + 
   geom_bar(aes(fill=ThreatLevel), stat="identity")
+
+#read table with species separated by threat and continent
+df <- read.table(text='Count    ThreatLevel  Continent
+                3 LeastConcerned  NorthAm
+4 NearThreatened  NorthAm
+1 Vulnerable  NorthAm
+2 LeastConcerned  SouthAm
+2 NearThreatened  SouthAm
+2 Vulnerable  SouthAm
+1 Endangered  SouthAm
+2 LeastConcerned  Eu
+1 CriticallyEndangered  Eu
+5 LeastConcerned  Asia
+5 NearThreatened  Asia
+5 Vulnerable  Asia
+5 Endangered  Asia
+3 LeastConcerned  Africa
+3 NearThreatened  Africa
+3 Vulnerable  Africa
+1 NearThreatened  Aus
+1 Vulnerable  Aus', header=TRUE)
+
+#plot using ggplot
+require(ggplot2)
+p <- ggplot(df, aes(Continent, Count, fill=ThreatLevel)) + 
+    geom_bar(stat='identity') +
+    theme_bw(base_size=16)
+print(p)
