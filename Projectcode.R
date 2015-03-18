6af0027ccb23fe09e36ae733774d836637bb63ae
Eunice Choi <euniceschoi93@gmail.com>
2015-03-18 22:37:09 +0000

    Threat Level Analysis

--- Projectcode.R
+++ Projectcode.R
@@ -9,4 +9,18 @@ library("ggplot2")
 ggplot(data=df, aes(x=PopulationTrend, y=Count)) + 
   geom_bar(aes(fill=PopulationTrend), stat="identity")
 
+#define read.table for threatlevel count 
+df <- read.table(text='Count    ThreatLevel
+9  Vulnerable
+7 Endangered
+1 CriticallyEndangered
+9 NearThreatened
+11  LeastConcerned', header=TRUE)
 
+library("ggplot2")
+#order x axis values
+ThreatLevel <- c("LeastConcerned", "NearThreatened", "Vulnerable", "Endangered", "CriticallyEndangered")
+#plot threatlevel trend onto bar graph
+ggplot(data=df, aes(x = ThreatLevel, y=Count)) + 
+  scale_x_discrete(limits = ThreatLevel) + 
+  geom_bar(aes(fill=ThreatLevel), stat="identity")
